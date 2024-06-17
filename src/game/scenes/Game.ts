4e5a6466fbbdb80e9d0c0cba28b3b1f2c600b732
Rishav Rout <rout.rishav@gmail.com>
2024-06-16 20:13:05 -0700

    decent

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -5,9 +5,85 @@ export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
+    tileBag: TileBag;
+    currentHand: Tile[] = [];
+    currentHandv2: Phaser.GameObjects.Container;
 
     constructor() {
         super('Game');
+    }
+
+    renderTile(tile: Tile): Phaser.GameObjects.Container {
+        const rendering = this.add.container();
+
+        const fontSize = 60;
+        const bg = this.add.rectangle(
+            fontSize * 0.4,
+            fontSize / 2,
+            fontSize,
+            fontSize * 0.9,
+            0xccfccb
+        );
+        rendering.add(bg);
+        rendering.add(
+            this.add.text(0, 0, tile.key === ' ' ? ' ' : tile.displayName, {
+                fontSize: fontSize,
+                color: '#261447',
+            })
+        );
+        rendering.add(
+            this.add.text(
+                fontSize * 0.6,
+                fontSize * 0.7,
+                tile.pointValue.toString(),
+                {
+                    fontSize: fontSize * 0.25,
+                    color: '#261447',
+                }
+            )
+        );
+        rendering.setSize(fontSize, fontSize);
+        bg.setInteractive();
+        bg.on('pointerover', () => {
+            bg.fillColor = 0x96e6b3;
+        });
+        bg.on('pointerout', () => {
+            bg.fillColor = 0xccfccb;
+        });
+        return rendering;
+    }
+
+    renderButtons() {
+        const {width, height} = this.cameras.default;
+        const shuffleButton = this.add.text(
+            width / 2,
+            height * 0.7,
+            'Shuffle',
+            {
+                color: 'white',
+            }
+        );
+        shuffleButton.setInteractive();
+        shuffleButton.on('pointerdown', () => {
+            shuffle(this.currentHand);
+            this.renderHand();
+        });
+        shuffleButton.on('pointerover', () => {
+            shuffleButton.setColor('#96e6b3');
+        });
+        shuffleButton.on('pointerout', () => {
+            shuffleButton.setColor('white');
+        });
+    }
+
+    renderHand() {
+        const {width, height} = this.cameras.default;
+        this.currentHandv2 = this.add.container(width / 2, height / 2);
+        this.currentHand.forEach((t, idx) => {
+            const rendered = this.renderTile(t);
+            rendered.x = (idx - 3) * 100;
+            this.currentHandv2.add(rendered);
+        });
     }
 
     create() {
@@ -15,18 +91,11 @@ export class Game extends Scene {
         const {width, height} = this.cameras.default;
         this.add.rectangle(width / 2, height / 2, width, height, 0x1b3022);
 
-        this.gameText = this.add
-            .text(512, 384, 'Basic something', {
-                fontFamily: 'Arial Black',
-                fontSize: 38,
-                color: '#ffffff',
-                // stroke: '#000000',
-                // strokeThickness: 8,
-                align: 'center',
-            })
-            .setOrigin(0.5)
-            .setDepth(100);
-        scrabbleStuff();
+        // scrabble stuff
+        this.tileBag = new TileBag();
+        this.currentHand = this.tileBag.popNRandomTiles(7);
+        this.renderHand();
+        this.renderButtons();
 
         EventBus.emit('current-scene-ready', this);
     }
@@ -82,14 +151,27 @@ class TileBag {
         }
     }
 
-    popRandomTile() {
+    shuffle() {
         this.keyList = shuffle(this.keyList);
+    }
+
+    popRandomTile() {
+        this.shuffle();
         const key = this.keyList.pop();
         if (!key) {
             throw new Error('Cannot pop tile with no tiles.');
         }
         this.incrementTile(key, -1);
         return this.getTile(key)!;
+    }
+
+    popNRandomTiles(n: number) {
+        this.shuffle();
+        const tiles = this.keyList.splice(0, n);
+        return tiles.map(t => {
+            this.incrementTile(t, -1);
+            return this.getTile(t)!;
+        });
     }
 
     constructor() {
@@ -156,18 +238,10 @@ class TileBag {
         });
     }
 }
-
-const shuffle = (array: string[]) => {
+function shuffle<T>(array: T[]) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-};
-
-function scrabbleStuff() {
-    const tileBag = new TileBag();
-    console.log(tileBag.popRandomTile());
-
-    return tileBag;
 }
